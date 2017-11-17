b4a73e50dc65e974c055699e8a14238174259035
tkerns1965 <tony.kerns.65@gmail.com>
2017-11-17 10:03:03 -0500

    Delete certbot_renew.sh

--- nginx_rp/certbot_renew.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# certbot certonly --webroot -n --staging --dry-run -w /var/www/html -d tkhome.ignorelist.com,owncloud.tkhome.ignorelist.com
-certbot renew --staging --dry-run
-# certbot renew
