dc84d61725a94bd9fab694e84dfefdfec7664e9c
tkerns1965 <tony.kerns.65@gmail.com>
2017-11-17 11:08:24 -0500

    Update certbot_create.sh

--- nginx_rp/root/certbot_create.sh
+++ nginx_rp/root/certbot_create.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-certbot certonly --webroot -n --staging --dry-run --agree-tos -m tony.kerns.65@gmail.com -w /var/www/html -d tkhome.ignorelist.com,owncloud.tkhome.ignorelist.com
+# certbot certonly --webroot -n --staging --dry-run --agree-tos -m tony.kerns.65@gmail.com -w /var/www/html -d tkhome.ignorelist.com,owncloud.tkhome.ignorelist.com
+certbot certonly --webroot -n --staging --agree-tos -m tony.kerns.65@gmail.com -w /var/www/html -d tkhome.ignorelist.com,owncloud.tkhome.ignorelist.com
 # certbot certonly --webroot -n --agree-tos -m tony.kerns.65@gmail.com -w /var/www/html -d tkhome.ignorelist.com,owncloud.tkhome.ignorelist.com
