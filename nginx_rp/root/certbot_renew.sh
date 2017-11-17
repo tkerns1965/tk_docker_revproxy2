094f9ac2e019b4dc74a22551f6aab2693ee4c078
tkerns1965 <tony.kerns.65@gmail.com>
2017-11-17 11:08:59 -0500

    Update certbot_renew.sh

--- nginx_rp/root/certbot_renew.sh
+++ nginx_rp/root/certbot_renew.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 # certbot certonly --webroot -n --staging --dry-run -w /var/www/html -d tkhome.ignorelist.com,owncloud.tkhome.ignorelist.com
-certbot renew --staging --dry-run
+# certbot renew --staging --dry-run
+certbot renew --staging
 # certbot renew
